cd37db8e258756e2bf5312acd0d70c8579fd434e
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-04-11 16:46:54 +0530

    Added flex for buttons

--- frontend-idx/src/pages/CreateProject.jsx
+++ frontend-idx/src/pages/CreateProject.jsx
@@ -1,38 +1,7 @@
-import { Button, Layout } from "antd";
+import { Button, Col, Flex, Row } from "antd";
 import { useCreteProject } from "../hooks/apis/mutations/useCreateProject";
 
-const layoutStyle = {
-    borderRadius: 8,
-    overflow: 'hidden',
-    width: 'calc(50% - 8px)',
-    maxWidth: 'calc(50% - 8px)'
-  }
-
-  const headerStyle = {
-    textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
-  };
-
-  const contentStyle = {
-    textAlign: 'center',
-    minHeight: 120,
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#0958d9',
-  };
-
-  const footerStyle = {
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#4096ff',
-  };
-
 const CreateProject = () => {
-  const { Header, Footer, Content } = Layout;
 
   const { createProjectMutation } = useCreteProject();
 
@@ -47,15 +16,15 @@ const CreateProject = () => {
   }
 
   return (
-    <Layout style={layoutStyle}>
-      <Header style={headerStyle}>Create Project</Header>
-      <Content style={contentStyle}>
-        <Button type="default" onClick={handleCreateProject}>
-          Create Playground
-        </Button>
-      </Content>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
+    <Row>
+        <Col span={24}>
+            <Flex justify="center" align="center">
+                <Button type="default" onClick={handleCreateProject}>
+                    Create Playground
+                </Button>
+            </Flex>
+        </Col>
+    </Row>
   );
 };
 
